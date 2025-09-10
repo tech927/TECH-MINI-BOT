0c0ed66edc8445fa2ec0c2472b48c18ba5c357a6
tech927 <onnui688@gmail.com>
2025-09-10 13:05:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 const express = require('express');
-const app = express();
-__path = process.cwd()
+const path = require('path');
 const bodyParser = require("body-parser");
+const app = express();
 const PORT = process.env.PORT || 8000;
-let code = require('./pair'); 
 
+// Import du module pair.js
+const code = require('./pair'); 
+
+// Augmenter le nombre max d'écouteurs pour éviter les warnings
 require('events').EventEmitter.defaultMaxListeners = 500;
 
-app.use('/code', code);
-app.use('/pair', async (req, res, next) => {
-    res.sendFile(__path + '/pair.html')
-});
-app.use('/', async (req, res, next) => {
-    res.sendFile(__path + '/main.html')
-});
-
+// Middleware pour parser le corps des requêtes
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Route pour le module pair
+app.use('/code', code);
+
+// Servir pair.html
+app.get('/pair', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pair.html'));
+});
+
+// Servir main.html depuis le dossier public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Fallback pour la racine
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'main.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`
 Don't Forget To Give Star ‼️
 
-
-Server running on http://localhost:` + PORT)
+Server running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
